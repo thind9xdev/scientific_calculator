c2fe0a29da07dd6612b3e40fda8b5cdd44f66c0e
Google <opensource@google.com>
2024-03-06 22:52:18 +0700

    fix some bug

--- src/App.tsx
+++ src/App.tsx
@@ -20,50 +20,76 @@ import "./App.css";
 const App: React.FC = () => {
   const [input, setInput] = useState<string>("");
   const [output, setOutput] = useState<string>("");
-  const handleClick = (value: string): void => {
-    if (value === "=") {
-      let expression: string = input;
-      expression = expression.replace(/(\d)\(/g, "$1*(");
-      expression = expression.replace(/\)(\d)/g, ")*$1");
-      try {
-        setOutput(eval(expression).toString());
-      } catch (error) {
-        setOutput("Lỗi khi thực hiện");
-      }
-    } else if (value === "C") {
-      setInput("");
-      setOutput("");
-    } else if (value === "sqrt") {
-      setInput((prevInput) => prevInput + "Math.sqrt(");
-    } else if (value === "cbrt") {
-      setInput((prevInput) => prevInput + "Math.cbrt(");
-    } else if (value === "(") {
-      setInput((prevInput) => prevInput + "(");
-    } else if (value === "cos") {
-      setInput((prevInput) => prevInput + "Math.cos(");
-    } else if (value === "tan") {
-      setInput((prevInput) => prevInput + "Math.tan(");
-    } else if (value === "asin") {
-      setInput((prevInput) => prevInput + "Math.asinh(");
-    } else if (value === "x") {
-      setInput((prevInput) => prevInput + "x");
-    } else if (value === "square") {
-      setInput((prevInput) => prevInput + "**2");
-    } else if (value === "cube") {
-      setInput((prevInput) => prevInput + "**3");
-    } else if (value === "power") {
-      setInput((prevInput) => prevInput + "**");
+  const [isRad, setIsRad] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  const rad = (degree: number) => {
+    return degree * (Math.PI / 180);
+  };
+
+  function factorial(n: number): number {
+    if (n === 0) {
+      return 1;
     } else {
-      setInput((prevInput) => prevInput + value);
+      return n * factorial(n - 1);
+    }
+  }
+
+  const handleClick = (value: string): void => {
+    rad(0);
+    factorial(0);
+    const operators: { [key: string]: string } = {
+      "=": "",
+      C: "",
+      sqrt: "√",
+      cbrt: "∛",
+      sin: `Math.sin(${isRad ? "rad(" : ""}`,
+      cos: `Math.cos(${isRad ? "rad(" : ""}`,
+      tan: `Math.tan(${isRad ? "rad(" : ""}`,
+      asin: `Math.asinh(${isRad ? "rad(" : ""}`,
+      "X!": "!",
+      x: "x",
+      square: "**2",
+      cube: "**3",
+      power: "**",
+      π: "Math.PI",
+      "(": "(",
+      ")": ")",
+    };
+    setInput((prevInput) => prevInput + (operators[value] ?? value));
+  };
+
+  const handleDelete = (): void => {
+    setInput((prevInput) => prevInput.slice(0, -1));
+    setOutput("");
+  };
+
+  const handleDeleteAll = (): void => {
+    setInput("");
+    setOutput("");
+  };
+  const handleCalculate = (): void => {
+    let expression: string = input;
+
+    expression = expression.replace(/(\d)\(/g, "$1*(");
+    expression = expression.replace(/\)(\d)/g, ")*$1");
+
+    expression = expression.replace(/√(\d+)/g, (_, number) => {
+      return Math.sqrt(Number(number)).toString();
+    });
+    expression = expression.replace(/∛(\d+)/g, (_, number) => {
+      return Math.cbrt(Number(number)).toString();
+    });
+
+    try {
+      setOutput(eval(expression).toString());
+      setIsError(false);
+    } catch (error: any) {
+      setOutput("Lỗi khi thực hiện");
+      setIsError(true);
     }
   };
-  // const handleClickMenu = () =>{
 
-  // }
-  const handleDelete = (): void => {
-    setInput((prevInput) => prevInput.slice(0, -1)); // Xóa ký tự cuối cùng khỏi chuỗi
-    setOutput(""); // Xóa kết quả
-  };
   return (
     <div className="calculator">
       <input
@@ -75,49 +101,129 @@ const App: React.FC = () => {
       />
       <input
         type="text"
+        style={
+          isError
+            ? {
+                fontSize: "13px",
+                color: "red",
+              }
+            : {}
+        }
         className="output"
         value={output}
         placeholder="Kết quả"
         readOnly
       />
+      <p style={{ fontSize: "12px" }} className="model">
+        {isRad ? "Rad" : "Deg"}
+      </p>
       <div className="buttons">
-        <button onClick={() => handleClick("(")}>(</button>
-        <button onClick={() => handleClick(")")}>)</button>
-        <button onClick={() => handleClick("sqrt")}>√</button>
-        <button onClick={() => handleClick("cbrt")}>∛</button>
-        <button onClick={() => handleClick("sin")}>sin</button>
-        <button onClick={() => handleClick("cos")}>cos</button>
-        <button onClick={() => handleClick("tan")}>tan</button>
-        <button onClick={() => handleClick("asin")}>asin</button>
+        <button className="button_func" onClick={() => setIsRad(false)}>
+          Deg
+        </button>
+        <button className="button_func" onClick={() => setIsRad(true)}>
+          Rad
+        </button>
+        <button className="button_func" onClick={() => handleClick("X!")}>
+          X!
+        </button>
+        <button className="button_func" onClick={() => handleClick("π")}>
+          {" "}
+          π
+        </button>
+        <button className="button_func" onClick={() => handleClick("(")}>
+          (
+        </button>
+        <button className="button_func" onClick={() => handleClick(")")}>
+          )
+        </button>
+        <button className="button_func" onClick={() => handleClick("sqrt")}>
+          √
+        </button>
+        <button className="button_func" onClick={() => handleClick("cbrt")}>
+          ∛
+        </button>
+        <button className="button_func" onClick={() => handleClick("sin")}>
+          sin
+        </button>
+        <button className="button_func" onClick={() => handleClick("cos")}>
+          cos
+        </button>
+        <button className="button_func" onClick={() => handleClick("tan")}>
+          tan
+        </button>
+        <button className="button_func" onClick={() => handleClick("asin")}>
+          asin
+        </button>
         {/* <button onClick={() => handleClick("x")}>x</button> */}
-        <button onClick={() => handleClick("square")}>x²</button>
-        <button onClick={() => handleClick("cube")}>x³</button>
-        <button onClick={() => handleClick("power")}>^</button>
+        <button className="button_func" onClick={() => handleClick("square")}>
+          x²
+        </button>
+        <button className="button_func" onClick={() => handleClick("cube")}>
+          x³
+        </button>
+        <button className="button_func" onClick={() => handleClick("power")}>
+          ^
+        </button>
         {/* <button style={{backgroundColor:"green"}} onClick={() => handleClickMenu()}>menu</button>  */}
         <button
-          style={{ backgroundColor: "darkgray" }}
+          style={{ backgroundColor: "darkorange" }}
           onClick={() => handleDelete()}
         >
           Del
         </button>{" "}
         {/* Nút xóa */}
-        <button onClick={() => handleClick("7")}>7</button>
-        <button onClick={() => handleClick("8")}>8</button>
-        <button onClick={() => handleClick("9")}>9</button>
-        <button onClick={() => handleClick("/")}>/</button>
-        <button onClick={() => handleClick("4")}>4</button>
-        <button onClick={() => handleClick("5")}>5</button>
-        <button onClick={() => handleClick("6")}>6</button>
-        <button onClick={() => handleClick("*")}>*</button>
-        <button onClick={() => handleClick("1")}>1</button>
-        <button onClick={() => handleClick("2")}>2</button>
-        <button onClick={() => handleClick("3")}>3</button>
-        <button onClick={() => handleClick("-")}>-</button>
-        <button onClick={() => handleClick("0")}>0</button>
-        <button onClick={() => handleClick(".")}>.</button>
-        <button onClick={() => handleClick("+")}>+</button>
-        <button onClick={() => handleClick("C")}>C</button>
-        <button onClick={() => handleClick("=")}>=</button>
+        <button className="button_number" onClick={() => handleClick("7")}>
+          7
+        </button>
+        <button className="button_number" onClick={() => handleClick("8")}>
+          8
+        </button>
+        <button className="button_number" onClick={() => handleClick("9")}>
+          9
+        </button>
+        <button className="button_func" onClick={() => handleClick("/")}>
+          /
+        </button>
+        <button className="button_number" onClick={() => handleClick("4")}>
+          4
+        </button>
+        <button className="button_number" onClick={() => handleClick("5")}>
+          5
+        </button>
+        <button className="button_number" onClick={() => handleClick("6")}>
+          6
+        </button>
+        <button className="button_func" onClick={() => handleClick("*")}>
+          *
+        </button>
+        <button className="button_number" onClick={() => handleClick("1")}>
+          1
+        </button>
+        <button className="button_number" onClick={() => handleClick("2")}>
+          2
+        </button>
+        <button className="button_number" onClick={() => handleClick("3")}>
+          3
+        </button>
+        <button className="button_func" onClick={() => handleClick("-")}>
+          -
+        </button>
+        <button className="button_number" onClick={() => handleClick("0")}>
+          0
+        </button>
+        <button className="button_func" onClick={() => handleClick(".")}>
+          .
+        </button>
+        <button className="button_func" onClick={() => handleClick("+")}>
+          +
+        </button>
+        <button className="button_func_del" onClick={() => handleDeleteAll()}>
+          C
+        </button>
+        <button className="button_func_equal" onClick={() => handleCalculate()}>
+          =
+        </button>
       </div>
     </div>
   );
